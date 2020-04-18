54d9f3293b00d8c12fd3c18f79cf6db9ad746005
Josiah Lansford <redrobbin99@gmail.com>
2020-04-18 12:37:18 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <header>
+        <h1>React Cache Simulator</h1>
+        <p>(c) 2020 Josiah R Lansford</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
