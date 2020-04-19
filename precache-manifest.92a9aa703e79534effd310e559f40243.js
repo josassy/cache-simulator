f44543f3a5e6c82416b836e1b9df291f988e7fdc
Josiah Lansford <redrobbin99@gmail.com>
2020-04-18 21:13:48 -0400

    Updates

---
+++ precache-manifest.92a9aa703e79534effd310e559f40243.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67e441c5ccb72e461d6e5461ad8b4241",
+    "url": "/cache-simulator/index.html"
+  },
+  {
+    "revision": "472f316f9a8a98f3c528",
+    "url": "/cache-simulator/static/css/main.e9867400.chunk.css"
+  },
+  {
+    "revision": "1a3f675b54759007a5d7",
+    "url": "/cache-simulator/static/js/2.bf02416e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/cache-simulator/static/js/2.bf02416e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "472f316f9a8a98f3c528",
+    "url": "/cache-simulator/static/js/main.70e48189.chunk.js"
+  },
+  {
+    "revision": "93a82d5160ea68a81f4e",
+    "url": "/cache-simulator/static/js/runtime-main.447693bf.js"
+  }
+]);
